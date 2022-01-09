c5e6fc6ee85e954146adf976a2cbc52914c471a8
jjanin <jasper.janin@ugent.be>
2022-01-09 01:12:45 +0100

    working

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # rave-bot
 i hab cool lamp
+
+Required: .env file with content:
+```
+TOKEN="<discord bot token>"
+BRIDGE-IP="<hue bridge ip>"
+BRIDGE-LOGIN="<username for hue bridge>"
+GUILDS="[<guild>, <id's>, <where>, <bot>, <may>, <listen>]"
+```
--- main.py
+++ main.py
@@ -1,5 +1,63 @@
 from LampController import lampController
-from LampController import color
+from LampController import color as c
 from RaveBot import raveBot
 
-raveBot.test()
+from dotenv import load_dotenv
+import os
+import json
+
+import discord
+from discord.commands import Option
+from discord.commands.errors import ApplicationCommandInvokeError
+
+load_dotenv()
+
+TOKEN = os.getenv("DISCORD-BOT-TOKEN")
+IP = os.getenv("BRIDGE-IP")
+AUTH = os.getenv("BRIDGE-LOGIN")
+GUILDS = json.loads(os.getenv("GUILDS"))
+
+lamp = lampController.Lamp(IP, AUTH)
+
+bot = discord.Bot()
+
+
+@bot.command(guild_ids=GUILDS)
+async def color(
+    ctx,
+    color: Option(str, "Choose a color", choices=c.colorChoices.keys())
+):
+    """Set Jaspers lamp color/status"""
+    try:
+        await lamp.setLight(c.colorChoices[color])
+    except:
+        pass
+
+    message = ""
+    if color == "Off":
+        message = "Turned lamp off"
+    else:
+        message = f"Lamp set to {color}"
+    await ctx.respond(message)
+
+
+@bot.command(guild_ids=GUILDS)
+async def rgb(
+    ctx,
+    r: int,
+    g: int,
+    b: int
+):
+    """Set Jaspers lamp color by RGB values"""
+    r = max(0, min(255, r))
+    g = max(0, min(255, g))
+    b = max(0, min(255, b))
+    try:
+        await lamp.setLight(c.rgb(r, g, b))
+    except:
+        pass
+
+    message = f"Lamp set to {r}, {g}, {b}"
+    await ctx.respond(message)
+
+bot.run(TOKEN)
